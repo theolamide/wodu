813e597156ad0baa98fc45befa9c419de4f1d0ad
theolamide <ajadidan@gmail.com>
2022-02-13 14:20:22 -0500

    Keyboard styling and home page spacing styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './App.css';
 
 import Helpers, {attempts, answerDictionary} from './components/HelpersFunctions'
-import Grid from './components/Grid';
 import Keyboard from './components/Keyboard';
 
 function App() {
@@ -40,22 +39,24 @@ function App() {
     <div className="App">
           {"wọ́dù".toUpperCase()}
           <div className="full-grid">
-            {attemptDict[1].map((char, index)=> <p className={`single-grid ${answerDictionaryState[1]===null ? '' : answerDictionaryState[1][index]}`} key={index}>{char}</p>)}
-          </div>
-          <div className="full-grid">
-            {attemptDict[2].map((char, index)=> <p className={`single-grid ${answerDictionaryState[2]===null ? '' : answerDictionaryState[2][index]}`} key={index}>{char}</p>)}
-          </div>
-          <div className="full-grid">
-            {attemptDict[3].map((char, index)=> <p className={`single-grid ${answerDictionaryState[3]===null ? '' : answerDictionaryState[3][index]}`} key={index}>{char}</p>)}
-          </div>
-          <div className="full-grid">
-            {attemptDict[4].map((char, index)=> <p className={`single-grid ${answerDictionaryState[4]===null ? '' : answerDictionaryState[4][index]}`} key={index}>{char}</p>)}
-          </div>
-          <div className="full-grid">
-            {attemptDict[5].map((char, index)=> <p className={`single-grid ${answerDictionaryState[5]===null ? '' : answerDictionaryState[5][index]}`} key={index}>{char}</p>)}
-          </div>
-          <div className="full-grid">
-            {attemptDict[6].map((char, index)=> <p className={`single-grid ${answerDictionaryState[6]===null ? '' : answerDictionaryState[6][index]}`} key={index}>{char}</p>)}
+            <div className="grid-row">
+              {attemptDict[1].map((char, index)=> <p className={`single-grid ${answerDictionaryState[1]===null ? '' : answerDictionaryState[1][index]}`} key={index}>{char}</p>)}
+            </div>
+            <div className="grid-row">
+              {attemptDict[2].map((char, index)=> <p className={`single-grid ${answerDictionaryState[2]===null ? '' : answerDictionaryState[2][index]}`} key={index}>{char}</p>)}
+            </div>
+            <div className="grid-row">
+              {attemptDict[3].map((char, index)=> <p className={`single-grid ${answerDictionaryState[3]===null ? '' : answerDictionaryState[3][index]}`} key={index}>{char}</p>)}
+            </div>
+            <div className="grid-row">
+              {attemptDict[4].map((char, index)=> <p className={`single-grid ${answerDictionaryState[4]===null ? '' : answerDictionaryState[4][index]}`} key={index}>{char}</p>)}
+            </div>
+            <div className="grid-row">
+              {attemptDict[5].map((char, index)=> <p className={`single-grid ${answerDictionaryState[5]===null ? '' : answerDictionaryState[5][index]}`} key={index}>{char}</p>)}
+            </div>
+            <div className="grid-row">
+              {attemptDict[6].map((char, index)=> <p className={`single-grid ${answerDictionaryState[6]===null ? '' : answerDictionaryState[6][index]}`} key={index}>{char}</p>)}
+            </div>
           </div>
         {/* <Grid nameOfTheDay={nameOfTheDay} /> */}
         <Keyboard 
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,25 +1,40 @@
 import React from "react";
+import { FiDelete } from 'react-icons/fi'
+// FiDelete
 
 const Keyboard = ({buttonClick}) => {
 
     let characters = [['w','è', 'e', 'é', 'r', 't', 'y', 'u', 'i', 'ò', 'o', 'ó', 'p'], ['à', 'a', 'á', 's', 'ṣ', 'd', 'f', 'g', 'gb', 'h', 'j', 'k', 'l'], ['enter', 'ẹ̀', 'ẹ', 'ẹ́', 'b', 'm', 'n', 'ọ̀', 'ọ', 'ọ́', '<']]
 
     return(
-        <div>
-            Keyboard Component
-            <div>
+            <div 
+            style={{
+                // border: '1px solid black', 
+                height: '200px', 
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+
+            }}>
                 {characters.map((arr, index) => 
-                <div key={index}>
+                <div key={index} 
+                style={{
+                        // border: '1px solid red', 
+                        height: '30%'}}>
                     {arr.map((element, idx) => 
-                        <button key={idx} onClick={()=> buttonClick(element)}>
-                            {element === '<' ? <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 0 25 25" width="25">
-    <path fill="black" d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"></path>
-  </svg> : element.toUpperCase()}
+                        <button key={idx} onClick={()=> buttonClick(element)} 
+                        style={{
+                            // border: '1px solid green',
+                            height: '58px',
+                            width: '50px',
+                            flex: '1',
+                            margin: '0 6px 0 0'
+                        }}>
+                            {element === '<' ?  <FiDelete />: element.toUpperCase()}
                         </button>
                         )}
                 </div>)}
             </div>
-        </div>
     )
 }
 
