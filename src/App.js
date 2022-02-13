20a6e846c43a6abd794be77d2d0d2c461dfe3c69
theolamide <ajadidan@gmail.com>
2022-02-13 11:09:10 -0500

    deleting setup and working correctly

--- src/App.js
+++ src/App.js
@@ -7,27 +7,30 @@ import Keyboard from './components/Keyboard';
 
 function App() {
   let nameOfTheDay = "OLAMI"
-  const [answerDictionaryHere, setAnswerDictionaryHere] = useState(localStorage.getItem("answerDictionary") || answerDictionary)
+  const [answerDictionaryState, setAnswerDictionaryState] = useState((typeof(localStorage.getItem("answerDictionary")) === "object" ? localStorage.getItem("answerDictionary") : JSON.parse(localStorage.getItem("answerDictionary"))) || answerDictionary)
+  console.log("ANSWER DIC BACKGROUNDS", answerDictionaryState)
   const [currentAttKey, setCurrentAttKey] = useState(JSON.parse(localStorage.getItem("currentAtt")) || 1) // This refers to the key to the current row being attempted
   const [currentIndex, setCurrentIndex] = useState(JSON.parse(localStorage.getItem("currentIndex")) || 0) // This referes to the current index of the row being attempted
   const [attemptDict, setAttemptDict] = useState(JSON.parse(localStorage.getItem("attemptDict")) || attempts ) // This is the whole attempt object. The whole grid, if you will.
 
   const buttonClick = (element) => {
     if (element === 'enter'){
-      Helpers.answerCheck(currentAttKey, nameOfTheDay, setAnswerDictionaryHere, setCurrentAttKey, setCurrentIndex)
+      Helpers.answerCheck(currentAttKey, nameOfTheDay, setAnswerDictionaryState, setCurrentAttKey, setCurrentIndex)
     } else if (element === '<'){
-      return
+      Helpers.deleteCharacter(currentAttKey, currentIndex, setCurrentIndex, setAttemptDict, nameOfTheDay.length)
+      setAttemptDict(JSON.parse(localStorage.getItem("attemptDict")))
     } else {
+      if (Helpers.rowComplete(currentAttKey)) return
       let tempAttemptDict = {...attemptDict}
       let currentAttArr = tempAttemptDict[currentAttKey]
       // If already at the end, and element not "<", or "enter", do nothing.
-      if (Helpers.rowComplete(currentAttKey)) return
-      console.log("Here after last item check")
+      // console.log("Here after last item check")
 
       currentAttArr[currentIndex] = element.toUpperCase()
       tempAttemptDict[currentAttKey] = currentAttArr
       setCurrentIndex(currentIndex+1)
       setAttemptDict(tempAttemptDict)
+      localStorage.setItem("currentIndex", currentIndex)
       localStorage.setItem("attemptDict", JSON.stringify(tempAttemptDict))
     }
   }
@@ -37,22 +40,22 @@ function App() {
     <div className="App">
           {"wọ́dù".toUpperCase()}
           <div className="full-grid">
-            {attemptDict[1].map((char, index)=> <p className={`single-grid ${answerDictionaryHere[1]===null ? '' : answerDictionaryHere[1][index]}`} key={index}>{char}</p>)}
+            {attemptDict[1].map((char, index)=> <p className={`single-grid ${answerDictionaryState[1]===null ? '' : answerDictionaryState[1][index]}`} key={index}>{char}</p>)}
           </div>
           <div className="full-grid">
-            {attemptDict[2].map((char, index)=> <p className={`single-grid ${answerDictionaryHere[2]===null ? '' : answerDictionaryHere[2][index]}`} key={index}>{char}</p>)}
+            {attemptDict[2].map((char, index)=> <p className={`single-grid ${answerDictionaryState[2]===null ? '' : answerDictionaryState[2][index]}`} key={index}>{char}</p>)}
           </div>
           <div className="full-grid">
-            {attemptDict[3].map((char, index)=> <p className={`single-grid ${answerDictionaryHere[3]===null ? '' : answerDictionaryHere[3][index]}`} key={index}>{char}</p>)}
+            {attemptDict[3].map((char, index)=> <p className={`single-grid ${answerDictionaryState[3]===null ? '' : answerDictionaryState[3][index]}`} key={index}>{char}</p>)}
           </div>
           <div className="full-grid">
-            {attemptDict[4].map((char, index)=> <p className={`single-grid ${answerDictionaryHere[4]===null ? '' : answerDictionaryHere[4][index]}`} key={index}>{char}</p>)}
+            {attemptDict[4].map((char, index)=> <p className={`single-grid ${answerDictionaryState[4]===null ? '' : answerDictionaryState[4][index]}`} key={index}>{char}</p>)}
           </div>
           <div className="full-grid">
-            {attemptDict[5].map((char, index)=> <p className={`single-grid ${answerDictionaryHere[5]===null ? '' : answerDictionaryHere[5][index]}`} key={index}>{char}</p>)}
+            {attemptDict[5].map((char, index)=> <p className={`single-grid ${answerDictionaryState[5]===null ? '' : answerDictionaryState[5][index]}`} key={index}>{char}</p>)}
           </div>
           <div className="full-grid">
-            {attemptDict[6].map((char, index)=> <p className={`single-grid ${answerDictionaryHere[6]===null ? '' : answerDictionaryHere[6][index]}`} key={index}>{char}</p>)}
+            {attemptDict[6].map((char, index)=> <p className={`single-grid ${answerDictionaryState[6]===null ? '' : answerDictionaryState[6][index]}`} key={index}>{char}</p>)}
           </div>
         {/* <Grid nameOfTheDay={nameOfTheDay} /> */}
         <Keyboard 
--- src/components/HelpersFunctions.js
+++ src/components/HelpersFunctions.js
@@ -19,14 +19,46 @@ export const answerDictionary = {
 const Helpers = {
     rowComplete: (currentAttKey) => {
         let attemptDict = JSON.parse(localStorage.getItem("attemptDict")) || attempts
-        console.log("ATT", attemptDict, currentAttKey)
+        // console.log("ATT", attemptDict, currentAttKey)
         let currentAtt = attemptDict[currentAttKey]
         let lastElement = currentAtt[currentAtt.length - 1]
+        // console.log("LAST ELEMENT", lastElement)
         if(lastElement[0] === " "){
             return false
         }
 
         return true
+    },
+
+    deleteCharacter: (currentAttKey, currentIndex, setCurrentIndex, setAttemptDict, answerLength) => {
+        // console.log("CURRENT INDEX", currentIndex)
+        let answerAttemptDict = (JSON.parse(localStorage.getItem("attemptDict")) || attempts)
+        if (!Helpers.rowComplete(currentAttKey) && currentIndex >= 0){
+            // console.log("IN HERE")
+            let indexToDelete = answerAttemptDict[currentAttKey].indexOf(" ")
+            if (indexToDelete > 0){
+                indexToDelete = indexToDelete - 1
+            }
+            answerAttemptDict[currentAttKey][indexToDelete] = " "
+            setCurrentIndex(indexToDelete)
+            localStorage.setItem("currentIndex", indexToDelete)
+            // console.log("ANSWER ATTEMPT", answerAttemptDict)
+            localStorage.setItem("attemptDict", JSON.stringify(answerAttemptDict))
+            setAttemptDict(answerAttemptDict)
+        } else if (Helpers.rowComplete(currentAttKey) && answerAttemptDict[currentAttKey].length === answerLength) {
+            // console.log("IN HERE 2")
+            let indexToDelete = answerAttemptDict[currentAttKey].length - 1
+            answerAttemptDict[currentAttKey][indexToDelete] = " "
+            setCurrentIndex(indexToDelete)
+            localStorage.setItem("currentIndex", indexToDelete)
+            // console.log("ANSWER ATTEMPT", answerAttemptDict)
+            localStorage.setItem("attemptDict", JSON.stringify(answerAttemptDict))
+            setAttemptDict(answerAttemptDict)
+        }
+        // If at the begining, do nothing
+        // If row outside of grid, do nothing
+        // If in the middle, pop the last populated element
+        // Update current index
     },
 
     answerCheck: (currentAttKey, nameOfTheDay, setAnswerDictionaryHere, setCurrentAttKey, setCurrentIndex) => {
@@ -37,7 +69,7 @@ const Helpers = {
             let answer = nameOfTheDay.split('')
             let answerAttempt = (JSON.parse(localStorage.getItem("attemptDict")) || attempts)[currentAttKey]
             let answerColorArr = []
-            console.log("Before For Loop")
+            // console.log("Before For Loop")
             for (let i=0; i<answer.length; i++){
                 // console.log("ENTER FOR LOOP")
                 if (answer[i] === answerAttempt[i]) {
@@ -47,21 +79,22 @@ const Helpers = {
                 } else {
                     answerColorArr[i] = "absent"
                 }
-                console.log("COLOR ARR 1", answerColorArr)
+                // console.log("COLOR ARR 1", answerColorArr)
             }
-            console.log("COLOR ARR 2", answerColorArr)
-            answerDictionary[currentAttKey] = answerColorArr
+            // console.log("COLOR ARR 2", answerColorArr)
+            let answerDictionaryFromLocal = JSON.parse(localStorage.getItem("answerDictionary")) || answerDictionary
+            answerDictionaryFromLocal[currentAttKey] = answerColorArr
             let currentAtt = JSON.parse(localStorage.getItem("currentAtt")) || 1
             currentAtt += 1
-            localStorage.setItem("answerDictionary", JSON.stringify(answerDictionary))
+            localStorage.setItem("answerDictionary", JSON.stringify(answerDictionaryFromLocal))
             localStorage.setItem("currentAtt", JSON.stringify(currentAtt))
-            setAnswerDictionaryHere(answerDictionary)
+            setAnswerDictionaryHere(answerDictionaryFromLocal)
             setCurrentAttKey(currentAtt)
             setCurrentIndex(0)
-
+            localStorage.setItem("currentIndex", 0)
         }
 
-            // If no, do nothing
+        // Else, do nothing.
     },
 
 }
